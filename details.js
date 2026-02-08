899f64e877ec733870ca4ab00ae8e5019f441b46
CBatista <cabatist@uncg.edu>
2026-02-08 00:59:09 -0500

    Fixed region and role not showing in details

--- details.js
+++ details.js
@@ -8,6 +8,8 @@ const data = {
         name: "Viego, The Ruined King",
         image: "images/viego.jpg",
         description: "Once ruler of a long-lost kingdom, Viego perished over a thousand years ago when his attempt to bring his wife back from the dead triggered the magical catastrophe known as the Ruination. Transformed into a powerful, unliving specter tortured by an obsessive longing for his centuries-dead queen, Viego now stands as the Ruined King, controlling the deadly Harrowings as he scours Runeterra for anything that might one day restore her, and destroying all in his path as the Black Mist pours endlessly from his cruel, broken heart.",
+        region: "Shadow Isles",
+        role: "Fighter / Assassin",
         abilities: [
             { name: "Passive - Sovereign’s Domination", description:"Viego can temporarily possess enemy champions he helps kill, healing for a portion of their max health. During possession, Viego's items, attacks, and non-ultimate abilities become those of his slain enemy's, and he gains a free cast of his own ultimate, Heartbreaker. While possessing an enemy, Viego also gets bonus movement speed when moving toward enemy champions."},
             { name: "Q - Blade of the Ruined King", description:"Passive: Viego’s attacks deal a percent of the target's current health as bonus damage on-hit. When Viego attacks an enemy he recently hit with an ability, the attack strikes twice. The second strike siphons health from the target instead of dealing regular damage, but still applies on-hit effects and can critically strike. This passive is maintained during possession. \nActive: Viego stabs forward with his blade, damaging all enemies hit."},
@@ -21,6 +23,8 @@ const data = {
         name: "Kindred, The Eternal Hunters",
         image: "images/kindred.png",
         description: "Separate, but never parted, Kindred represents the twin essences of death. Lamb's bow offers a swift release from the mortal realm for those who accept their fate. Wolf hunts down those who run from their end, delivering violent finality within his crushing jaws. Though interpretations of Kindred's nature vary across Runeterra, every mortal must choose the true face of their death.",
+        region: "Runeterra",
+        role: "Marksman",
         abilities: [
             { name: "Passive - Mark of the Kindred", description:"Kindred can mark targets to Hunt. Successfully completing a Hunt permanently empowers Kindred's basic abilities. Every 4 hunts completed also increases Kindred's basic attack range."},
             { name: "Q - Dance of Arrows", description:"Kindred tumbles and shoots up to three arrows at nearby targets."},
@@ -35,6 +39,8 @@ const data = {
         name: "Diana, Scorn of the Moon",
         image: "images/diana.png",
         description: "Bearing her crescent moonblade, Diana fights as a warrior of the Lunari—a faith all but quashed in the lands around Mount Targon. Clad in shimmering armor the color of winter snow at night, she is a living embodiment of the silver moon’s power. Imbued with the essence of an Aspect from beyond Targon’s towering summit, Diana is no longer wholly human, and struggles to understand her power and purpose in this world.",
+        region: "Mount Targon",
+        role: "Fighter / Assassin",
         abilities: [
             { name: "Passive - Moonsilver Blade", description:"Every third strike cleaves nearby enemies for an additional magic damage. After casting a spell, Diana gains Attack Speed for 5 seconds."},
             { name: "Q - Crescent Strike", description:"Unleashes a bolt of lunar energy in an arc dealing magic damage. Afflicts enemies struck with Moonlight, revealing them if they are not stealthed for 3 seconds."},
@@ -51,6 +57,8 @@ const data = {
         name: "Morgana, The Fallen",
         image: "images/morgana.jpg",
         description: "Conflicted between her celestial and mortal natures, Morgana bound her wings to embrace humanity, and inflicts her pain and bitterness upon the dishonest and the corrupt. She rejects laws and traditions she believes are unjust, and fights for truth from the shadows of Demacia—even as others seek to repress it—by casting shields and chains of dark fire. More than anything else, Morgana truly believes that even the banished and outcast may one day rise again.",
+        region: "Demacia",
+        role: "Support / Mage",
         abilities: [
             { name: "Passive - Soul Siphon", description:"Morgana drains spirit from her enemies, healing as she deals damage to champions, large minions, and medium and larger jungler monsters."},
             { name: "Q - Dark Binding", description:"Morgana binds an enemy in place with dark magic, forcing them to feel the pain they've caused and dealing magic damage."},
@@ -64,6 +72,7 @@ const data = {
         name: "Jhin, The Virtuoso",
         image: "images/jhin.jpg",
         description: "Jhin is a meticulous criminal psychopath who believes murder is art. Once an Ionian prisoner, but freed by shadowy elements within Ionia's ruling council, the serial killer now works as their cabal's assassin. Using his gun as his paintbrush, Jhin creates works of artistic brutality, horrifying victims and onlookers. He gains a cruel pleasure from putting on his gruesome theater, making him the ideal choice to send the most powerful of messages: terror.",
+        role: "Marksman",
         abilities: [
             { name: "Passive - Whisper", description:"Jhin's hand cannon, Whisper, is a precise instrument designed to deal superior damage. It fires at a fixed rate and carries only four shots. Jhin imbues the final bullet with dark magics to critically strike and deal bonus execute damage. Whenever Whisper crits, it inspires Jhin with a burst of Move Speed."},
             { name: "Q - Dancing Grenade", description:"Jhin launches a magical cartridge at an enemy. It can hit up to four targets and gains damage each time it kills."},
@@ -99,6 +108,8 @@ if (c){
     document.getElementById("name").textContent = c.name;
     document.getElementById("image").src = c.image;
     document.getElementById("description").textContent = c.description;
+    document.getElementById("region").textContent = c.region;
+    document.getElementById("role").textContent = c.role;
 
     const abilityList = document.getElementById("abilities");
     abilityList.innerHTML = "";
